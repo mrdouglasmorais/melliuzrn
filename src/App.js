6e899321e28e601b09e2defe9442c6fce02ac043
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2021-11-01 23:01:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  useColorScheme,
+  View,
+  Pressable,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Section from './Section';
+import styles from './style';
+
+const App = () => {
+  const [test, setStest] = useState('Teste');
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Header />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Pressable onPressIn={() => setStest('Funciona')}>
+            <Text> Pressione aqui </Text>
+          </Pressable>
+          <Text>{test}</Text>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
